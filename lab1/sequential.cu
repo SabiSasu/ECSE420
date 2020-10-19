e82fcca8d369a0ddeb5f655001f1dc48401d5c28
SabiSasu <sabina.sasu@gmail.com>
2020-10-19 11:22:14 -0400

    added timer to sequential

--- lab1/sequential.cu
+++ lab1/sequential.cu
@@ -19,38 +19,44 @@
 
 int process_sequential(int argc, char* argv[]) {
 
-	//if (argc != 4)
-	//	return 0;
+	if (argc != 4)
+		return 0;
 
 	// get arguments from command line
-	char* input_filename = "test_data\\input_10000.txt";//argv[1];
-	int file_length = 10000;//atoi(argv[2]);
-	char* output_filename = "output_data\\output_10000.txt";//argv[3];
+	char* input_filename = argv[1];
+	int file_length = atoi(argv[2]);
+	char* output_filename = argv[3];
 
 	FILE* input_file;
 	FILE* output_file;
 
 	if ((input_file = fopen(input_filename, "r")) == NULL) {
 		printf("Error! opening file");
-		// Program exits if file pointer returns NULL.
 		exit(1);
 	}
 
 	if ((output_file = fopen(output_filename, "w")) == NULL) {
 		printf("Error! opening file");
-		// Program exits if file pointer returns NULL.
 		exit(1);
 	}
-
+	
 	char line[256];
 	int i = 0;
 	int nums[3] = { 0 };
+
+	float memsettime;
+	cudaEvent_t start, stop;
+	cudaEventCreate(&start); cudaEventCreate(&stop);
+	cudaEventRecord(start, 0);
+	//start clock
+	clock_t begin = clock();
+
 	for (int i = 0; i < file_length; i++) {
 		fgets(line, sizeof(line), input_file);
 		int gateA = atoi(&line[0]);
 		int gateB = atoi(&line[2]);
 		int gateNum = atoi(&line[4]);
-		//printf("%d, %d, %d\n", gateA, gateB, gateNum);
+
 		int output = 0;
 		switch (gateNum) {
 		case AND: output = gateA & gateB;  break;
@@ -60,9 +66,17 @@ int process_sequential(int argc, char* argv[]) {
 		case XOR: output = ((!gateA & gateB) | (gateA & !gateB)); break;
 		case XNOR: output = !((!gateA & gateB) | (gateA & !gateB)); break;
 		}
-		//printf("%d\n", output);
 		fprintf(output_file, "%d\n", output);
 	}
+	//C timer library clock... doesnt work
+	clock_t end = clock();
+	long time_spent = ((double)end - begin) / CLOCKS_PER_SEC * 10000;
+	printf("Execution time: %f\n",  time_spent);
+	//cuda clock timer.... sorta works
+	cudaEventRecord(stop, 0); cudaEventSynchronize(stop);
+	cudaEventElapsedTime(&memsettime, start, stop);
+	printf("Cuda timer execution time %f milliseconds\n", memsettime);
+	cudaEventDestroy(start); cudaEventDestroy(stop);
 
 	fclose(input_file);
 	fclose(output_file);
